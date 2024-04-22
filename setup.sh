219b175ea04db9cf10cd9e75f6c52ef93af2a651
Bryan Cardwell <bryan.cardwell@gmail.com>
2024-04-22 16:46:57 -0400

    Add two more libraries to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 wget https://github.com/TinyCircuits/TinyCircuits-BMA250-library/archive/refs/heads/master.zip -O bma250.zip
 wget https://github.com/bkatona/Adafruit_SleepyDog/archive/refs/heads/main.zip -O sleepyDog.zip
 wget https://github.com/arduino-libraries/RTCZero/archive/refs/heads/master.zip -O rtcZero.zip
+wget https://github.com/greiman/SdFat/archive/refs/heads/master.zip -O sdFat.zip
+wget https://github.com/arduino-libraries/ArduinoLowPower/archive/refs/heads/master.zip -O arduinoLowPower.zip
