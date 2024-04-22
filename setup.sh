9b4418846b03da08945797d616423b5d4789268f
Bryan Cardwell <bryan.cardwell@gmail.com>
2024-04-22 15:49:00 -0400

    Add setup script to download libraries

---
+++ setup.sh
@@ -0,0 +1,3 @@
+wget https://github.com/TinyCircuits/TinyCircuits-BMA250-library/archive/refs/heads/master.zip -O bma250.zip
+wget https://github.com/bkatona/Adafruit_SleepyDog/archive/refs/heads/main.zip -O sleepyDog.zip
+wget https://github.com/arduino-libraries/RTCZero/archive/refs/heads/master.zip -O rtcZero.zip
